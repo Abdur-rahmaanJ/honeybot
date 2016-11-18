909177c6180cf9304355c9567f26f14105bca51f
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2016-11-18 21:20:03 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+VARR='here'
+
+wordpress_lib_installed = True
+
+
+key='65d4b4eabf8b06d5'
+
+IRC_SERVER ="chat.freenode.net"
+IRC_CHANNEL = "##bottestingmu"
+
+IRC_PORT = 6667
+NICKNAME = "appinventormuBot"
+
+OWNER1 = "appinventormu"
+OWNER2 = "appinv"
+
+OWNERS =[OWNER1,OWNER2]
+PASSWORD = "rfvtgbyhn"
---
+++ main.py
@@ -0,0 +1,18 @@
+import ft
+from ft import*
+
+
+#c = NNN()
+
+#c.pri2()
+
+abot = Honeybot()
+
+print(abot.name)
+abot.prvar()
+abot.connection()
+
+while 1:
+	pass
+	abot.pingChecker()
+	abot.printer()
